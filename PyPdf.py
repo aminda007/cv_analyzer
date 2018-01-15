1e0a63c7fa93e622bb031e27e2ef544ce7af1fad
Aminda Abeywardana <aminda.14@cse.mrt.ac.lk>
2018-01-15 23:38:58 +0530

    delete file

--- PyPdf.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-
-file = open('cv.pdf', 'rb')
-pdfreader = PyPDF2.PdfFileReader(file)
-print(pdfreader.getNumPages())
-pageObject = pdfreader.getPage(0)
-print(pageObject.extractText())
-
